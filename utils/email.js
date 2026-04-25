158ae812039ec9fb61333ef7103113c468fae43b
ahmedabdelelah22 <ahmedabdelelah22@githup.com>
2026-04-25 20:03:16 +0300

    Describe your recent changes

--- utils/email.js
+++ utils/email.js
@@ -28,13 +28,11 @@ class Email {
 
     // 🧪 In development, Gmail works just fine
     return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-     secure: false, // ✅ add this - no SSL for port 2525
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD, // 🔑 App password required
+      },
     });
   }
 
